bb811199afc164666825ed7516354248f242a88a
Sam <skysar@mtu.edu>
2018-08-09 13:53:11 -0400

    Cleaned up cpp file and created a param file.

--- src/main.cpp
+++ src/main.cpp
@@ -53,65 +53,69 @@ std::string frame_id;
 int main(int argc, char *argv[])
 {
 
-  // ROS node init
-  ros::init(argc, argv, "vectornav");
-  ros::NodeHandle n;
-  ros::NodeHandle pn("~");
-  pubIMU = n.advertise<sensor_msgs::Imu>("vectornav/IMU", 1000);
-  pubMag = n.advertise<sensor_msgs::MagneticField>("vectornav/Mag", 1000);
-  pubGPS = n.advertise<sensor_msgs::NavSatFix>("vectornav/GPS", 1000);
-  pubTemp = n.advertise<sensor_msgs::Temperature>("vectornav/Temp", 1000);
-  pubPres = n.advertise<sensor_msgs::FluidPressure>("vectornav/Pres", 1000);
+    // ROS node init
+    ros::init(argc, argv, "vectornav");
+    ros::NodeHandle n;
+    ros::NodeHandle pn("~");
+    pubIMU = n.advertise<sensor_msgs::Imu>("vectornav/IMU", 1000);
+    pubMag = n.advertise<sensor_msgs::MagneticField>("vectornav/Mag", 1000);
+    pubGPS = n.advertise<sensor_msgs::NavSatFix>("vectornav/GPS", 1000);
+    pubTemp = n.advertise<sensor_msgs::Temperature>("vectornav/Temp", 1000);
+    pubPres = n.advertise<sensor_msgs::FluidPressure>("vectornav/Pres", 1000);
 
-  pn.param<std::string>("frame_id", frame_id, "vectornav");
+    pn.param<std::string>("frame_id", frame_id, "vectornav");
 
-  // Serial Port Settings
-	string SensorPort;	
-	int SensorBaudrate;
+    // Serial Port Settings
+    string SensorPort;	
+    int SensorBaudrate;
 	
 	pn.param<std::string>("serial_port", SensorPort, "/dev/ttyUSB0");
 	pn.param<int>("serial_baud", SensorBaudrate, 115200);
 	
-  ROS_INFO("Connecting to : %s @ %d Baud", SensorPort.c_str(), SensorBaudrate);
+    ROS_INFO("Connecting to : %s @ %d Baud", SensorPort.c_str(), SensorBaudrate);
 
-	// Create a VnSensor object and connect to sensor
-	VnSensor vs;
-	vs.connect(SensorPort, SensorBaudrate);
+    // Create a VnSensor object and connect to sensor
+    VnSensor vs;
+    vs.connect(SensorPort, SensorBaudrate);
 
-	// Query the sensor's model number.
-	string mn = vs.readModelNumber();	
-  ROS_INFO("Model Number: %s", mn.c_str());
+    // Query the sensor's model number.
+    string mn = vs.readModelNumber();	
+    ROS_INFO("Model Number: %s", mn.c_str());
 
-	// Set Data output Freq [Hz]
-	int async_output_rate;
-	pn.param<int>("async_output_rate", async_output_rate, 40);
-	vs.writeAsyncDataOutputFrequency(async_output_rate);
-  
+    // Set Data output Freq [Hz]
+    int async_output_rate;
+    pn.param<int>("async_output_rate", async_output_rate, 40);
+    vs.writeAsyncDataOutputFrequency(async_output_rate);
   
 	// Configure binary output message
 	BinaryOutputRegister bor(
 		ASYNCMODE_PORT1,
 		1000 / async_output_rate,  // update rate [ms]
-		COMMONGROUP_TIMESTARTUP | COMMONGROUP_QUATERNION | COMMONGROUP_ANGULARRATE | COMMONGROUP_POSITION | COMMONGROUP_ACCEL | COMMONGROUP_MAGPRES,
+		COMMONGROUP_TIMESTARTUP 
+        | COMMONGROUP_QUATERNION 
+        | COMMONGROUP_ANGULARRATE 
+        | COMMONGROUP_POSITION 
+        | COMMONGROUP_ACCEL 
+        | COMMONGROUP_MAGPRES,
 		TIMEGROUP_NONE,
 		IMUGROUP_NONE,
 		GPSGROUP_NONE,
 		ATTITUDEGROUP_NONE,
 		INSGROUP_NONE);
 
-	vs.writeBinaryOutput1(bor);
-	vs.registerAsyncPacketReceivedHandler(NULL, BinaryAsyncMessageReceived);
+    vs.writeBinaryOutput1(bor);
+    vs.registerAsyncPacketReceivedHandler(NULL, BinaryAsyncMessageReceived);
 
 
-  // You spin me right round, baby
-  // Right round like a record, baby
-  // Right round round round
-  ros::spin();
+    // You spin me right round, baby
+    // Right round like a record, baby
+    // Right round round round
+    ros::spin();
 
 
-  // Node has been terminated
-	vs.unregisterAsyncPacketReceivedHandler();
-	vs.disconnect();
+    // Node has been terminated
+    vs.unregisterAsyncPacketReceivedHandler();
+    vs.disconnect();
 	return 0;
 }
 
@@ -122,103 +126,108 @@ int main(int argc, char *argv[])
 void BinaryAsyncMessageReceived(void* userData, Packet& p, size_t index)
 {
 	
-	if (p.type() == Packet::TYPE_BINARY)
-	{
-		// First make sure we have a binary packet type we expect since there
-		// are many types of binary output types that can be configured.
-		if (!p.isCompatible(
-			COMMONGROUP_TIMESTARTUP | COMMONGROUP_QUATERNION | COMMONGROUP_ANGULARRATE | COMMONGROUP_POSITION | COMMONGROUP_ACCEL | COMMONGROUP_MAGPRES,
-			TIMEGROUP_NONE,
-			IMUGROUP_NONE,
-			GPSGROUP_NONE,
-			ATTITUDEGROUP_NONE,
-			INSGROUP_NONE))
-			// Not the type of binary packet we are expecting.
-			return;
+    if (p.type() == Packet::TYPE_BINARY)
+    {
+        // First make sure we have a binary packet type we expect since there
+        // are many types of binary output types that can be configured.
+        if (!p.isCompatible(
+			COMMONGROUP_TIMESTARTUP 
+            | COMMONGROUP_QUATERNION 
+            | COMMONGROUP_ANGULARRATE 
+            | COMMONGROUP_POSITION 
+            | COMMONGROUP_ACCEL 
+            | COMMONGROUP_MAGPRES,
+            TIMEGROUP_NONE,
+            IMUGROUP_NONE,
+            GPSGROUP_NONE,
+            ATTITUDEGROUP_NONE,
+            INSGROUP_NONE))
+            // Not the type of binary packet we are expecting.
+            return;
 
 
-		// Unpack the packet
-		uint64_t timeStartup = p.extractUint64();
-		vec4f q = p.extractVec4f();
-		vec3f ar = p.extractVec3f();
-		vec3d lla = p.extractVec3d();
-		vec3f al = p.extractVec3f();
-		vec3f mag = p.extractVec3f();
-		float temp = p.extractFloat();
-		float pres = p.extractFloat();
+        // Unpack the packet
+        uint64_t timeStartup = p.extractUint64();
+        vec4f q = p.extractVec4f();
+        vec3f ar = p.extractVec3f();
+        vec3d lla = p.extractVec3d();
+        vec3f al = p.extractVec3f();
+        vec3f mag = p.extractVec3f();
+        float temp = p.extractFloat();
+        float pres = p.extractFloat();
 
 		
-		// Publish ROS Message
+        // Publish ROS Message
 		
-		// IMU
-		sensor_msgs::Imu msgIMU;
+        // IMU
+        sensor_msgs::Imu msgIMU;
 		
-		msgIMU.header.stamp = ros::Time::now();
-		msgIMU.header.frame_id = frame_id;
+        msgIMU.header.stamp = ros::Time::now();
+        msgIMU.header.frame_id = frame_id;
 		
-		msgIMU.orientation.x = q[0];
-		msgIMU.orientation.y = q[1];
-		msgIMU.orientation.z = q[2];
-		msgIMU.orientation.w = q[3];
+        msgIMU.orientation.x = q[0];
+        msgIMU.orientation.y = q[1];
+        msgIMU.orientation.z = q[2];
+        msgIMU.orientation.w = q[3];
 		
-		msgIMU.angular_velocity.x = ar[0];
-		msgIMU.angular_velocity.y = ar[1];
-		msgIMU.angular_velocity.z = ar[2];
+        msgIMU.angular_velocity.x = ar[0];
+        msgIMU.angular_velocity.y = ar[1];
+        msgIMU.angular_velocity.z = ar[2];
 		
-		msgIMU.linear_acceleration.x = al[0];
-		msgIMU.linear_acceleration.y = al[1];
-		msgIMU.linear_acceleration.z = al[2];
+        msgIMU.linear_acceleration.x = al[0];
+        msgIMU.linear_acceleration.y = al[1];
+        msgIMU.linear_acceleration.z = al[2];
 		
-    pubIMU.publish(msgIMU);
+        pubIMU.publish(msgIMU);
 
     
-    // Magnetic Field
-    sensor_msgs::MagneticField msgMag;
+        // Magnetic Field
+        sensor_msgs::MagneticField msgMag;
     
-    msgMag.header.stamp = msgIMU.header.stamp;
-    msgMag.header.frame_id = msgIMU.header.frame_id;
+        msgMag.header.stamp = msgIMU.header.stamp;
+        msgMag.header.frame_id = msgIMU.header.frame_id;
 
-    msgMag.magnetic_field.x = mag[0];
-    msgMag.magnetic_field.y = mag[1];
-    msgMag.magnetic_field.z = mag[2];
+        msgMag.magnetic_field.x = mag[0];
+        msgMag.magnetic_field.y = mag[1];
+        msgMag.magnetic_field.z = mag[2];
 
-    pubMag.publish(msgMag);
+        pubMag.publish(msgMag);
     
     
-    // GPS
-    sensor_msgs::NavSatFix msgGPS;
+        // GPS
+        sensor_msgs::NavSatFix msgGPS;
     
-    msgGPS.header.stamp = msgIMU.header.stamp;
-    msgGPS.header.frame_id = msgIMU.header.frame_id;
+        msgGPS.header.stamp = msgIMU.header.stamp;
+        msgGPS.header.frame_id = msgIMU.header.frame_id;
 
-    msgGPS.latitude = lla[0];
-    msgGPS.longitude = lla[1];
-    msgGPS.altitude = lla[2];
+        msgGPS.latitude = lla[0];
+        msgGPS.longitude = lla[1];
+        msgGPS.altitude = lla[2];
 
-    pubGPS.publish(msgGPS);
+        pubGPS.publish(msgGPS);
     
     
-    // Temperature
-    sensor_msgs::Temperature msgTemp;
+        // Temperature
+        sensor_msgs::Temperature msgTemp;
     
-    msgTemp.header.stamp = msgIMU.header.stamp;
-    msgTemp.header.frame_id = msgIMU.header.frame_id;
+        msgTemp.header.stamp = msgIMU.header.stamp;
+        msgTemp.header.frame_id = msgIMU.header.frame_id;
     
-    msgTemp.temperature = temp;
+        msgTemp.temperature = temp;
     
-    pubTemp.publish(msgTemp);
+        pubTemp.publish(msgTemp);
     
     
-    // Barometer
-    sensor_msgs::FluidPressure msgPres;
+        // Barometer
+        sensor_msgs::FluidPressure msgPres;
     
-    msgPres.header.stamp = msgIMU.header.stamp;
-    msgPres.header.frame_id = msgIMU.header.frame_id;
+        msgPres.header.stamp = msgIMU.header.stamp;
+        msgPres.header.frame_id = msgIMU.header.frame_id;
     
-    msgPres.fluid_pressure = pres;
+        msgPres.fluid_pressure = pres;
     
-    pubPres.publish(msgPres);
+        pubPres.publish(msgPres);
   
-	}
+    }
 }
 
