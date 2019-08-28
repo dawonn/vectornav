2e45ef12883bfc4b0cc212b50e63eae824526172
Joseph Curtis <jccurtis@lbl.gov>
2019-08-28 13:52:32 -0700

    Add check for INS Fix before building GPS/Odom msg.
    Otherwise the CompositeData object throws an error with a VN100 in the callback which doesn't come to the surface while running the rosnode but does cause a slowdown so the max IMU rate is closer to 40Hz. The issue arises from calling cd.insStatus() before checking that the insStatus exists.

--- src/main.cpp
+++ src/main.cpp
@@ -331,8 +331,8 @@ void BinaryAsyncMessageReceived(void* userData, Packet& p, size_t index)
     }
 
     // GPS
-    //if (cd.insStatus() == INSSTATUS_GPS_FIX)
-    //{
+    if (cd.hasInsStatus() && (cd.insStatus() == INSSTATUS_GPS_FIX))
+    {
         vec3d lla = cd.positionEstimatedLla();
 
         sensor_msgs::NavSatFix msgGPS;
@@ -390,7 +390,7 @@ void BinaryAsyncMessageReceived(void* userData, Packet& p, size_t index)
             }
             pubOdom.publish(msgOdom);
         }
-    //}
+    }
 
     // Temperature
     if (cd.hasTemperature())
