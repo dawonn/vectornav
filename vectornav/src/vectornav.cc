0c06004f38fe31b1f8ce733b24e27e315d4ddd69
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2022-12-21 10:09:15 -0500

    avoid segfault on exit

--- vectornav/src/vectornav.cc
+++ vectornav/src/vectornav.cc
@@ -177,6 +177,15 @@ public:
     }
   }
 
+  ~Vectornav()
+  {
+    if (reconnect_timer_) {
+      reconnect_timer_->cancel();
+      reconnect_timer_.reset();
+    }
+    vs_.disconnect();
+  }
+
 private:
   /**
    * set serial port to low latency async to avoid bunching up of callbacks
