bc259c3d901314ae575f3fa0e19ceae3f39612f6
Richard <richard@dexory.com>
2023-03-02 10:11:59 +0000

    NED to ENU conversion for raw readings

--- vectornav/src/vn_sensor_msgs.cc
+++ vectornav/src/vn_sensor_msgs.cc
@@ -180,8 +180,16 @@ private:
       q_ned2enu.setRPY(M_PI, 0.0, M_PI / 2);
       msg.orientation = toMsg(q_ned2enu * q);
 
-      msg.angular_velocity = msg_in->angularrate;
-      msg.linear_acceleration = msg_in->accel;
+      // NED to ENU conversion
+      // swap x and y and negate z
+      msg.angular_velocity.x = msg_in->angularrate.y;
+      msg.angular_velocity.y = msg_in->angularrate.x;
+      msg.angular_velocity.z = -msg_in->angularrate.z;
+
+      msg.linear_acceleration.x = msg_in->accel.y;
+      msg.linear_acceleration.y = msg_in->accel.x;
+      msg.linear_acceleration.z = -msg_in->accel.z;
+
 
       fill_covariance_from_param("orientation_covariance", msg.orientation_covariance);
       fill_covariance_from_param("angular_velocity_covariance", msg.angular_velocity_covariance);
@@ -195,8 +203,16 @@ private:
     {
       sensor_msgs::msg::Imu msg;
       msg.header = msg_in->header;
-      msg.angular_velocity = msg_in->imu_rate;
-      msg.linear_acceleration = msg_in->imu_accel;
+
+      // NED to ENU conversion
+      // swap x and y and negate z
+      msg.angular_velocity.x = msg_in->imu_rate.y;
+      msg.angular_velocity.y = msg_in->imu_rate.x;
+      msg.angular_velocity.z = -msg_in->imu_rate.z;
+
+      msg.linear_acceleration.x = msg_in->imu_accel.y;
+      msg.linear_acceleration.y = msg_in->imu_accel.x;
+      msg.linear_acceleration.z = -msg_in->imu_accel.z;
 
       fill_covariance_from_param("angular_velocity_covariance", msg.angular_velocity_covariance);
       fill_covariance_from_param(
